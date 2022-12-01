41378f4ad25c50ceec885dd2d71533eb5cdb8b82
Aryaman-13 <aryaman.khandelwal@plaksha.edu.in>
2022-12-02 02:53:13 +0530

    complete day1

---
+++ day1.py
@@ -0,0 +1,21 @@
+import numpy as np
+file1 = open("day1.txt","r")
+data = file1.read().split('\n')
+data = data +['']
+sum_list = []
+ele_list = []
+count = 0
+for i in range(len(data)):
+    if(data[i]!=''):
+        ele_list.append(int(data[i]))
+    else:
+        sum_list.append(sum(ele_list))
+        ele_list = []
+sum_list = np.sort(sum_list)
+total = sum_list[-1]+sum_list[-2]+sum_list[-3]
+print(total)
+
+
+
+
+
