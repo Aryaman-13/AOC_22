810ac765a001d8b0dc2e2eb9d3072f88418fdfea
Aryaman-13 <aryaman.khandelwal@plaksha.edu.in>
2022-12-03 08:28:44 +0530

    change the structure

--- day2.py
+++ day2.py
@@ -7,36 +7,27 @@ for i in range(len(data)):
     str = data[i]
     opp = str[0]
     you = str[2]
-    #now writing the if condition
-    if(opp =='A' and you =='X'):
-        #this results in draw-- both rock
-        score = 3 + 1
-    if(opp=='B' and you =='Y'):
-        #this results in draw -- both paper
-        score = 3 + 2
-    if(opp=='C' and you == 'Z'):
-        #this results in draw-- both scissor
-        score = 3 + 3
-    #now checking for other conditions---first where the opponent wins
-    if(opp=='A' and you =='Z'):
-        #I have taken scissor
-        score = 0 + 3
-    if(opp=='C'and you =='Y'):
-        #I have taken paper
-        score = 0 + 2
-    if(opp=='B'and you =='X'):
-        #I have taken rock
-        score = 0 + 1
-    #now taking the conditions where I am winning
-    if(opp=='A' and you =='Y'):
-        # I have taken paper
-        score = 6 + 2
-    if(opp=='B' and you == 'Z'):
-        #I have taken scissor
-        score = 6 + 3
-    if(opp=='C'and you == 'X'):
-        #I have taken rock
-        score = 6 + 1
+    #writing the dfferent conditions
+    if(opp=='A'):
+        if(you=='X'): #draw
+            score = 3+1
+        elif(you=='Z'): #lose
+            score = 0 + 3
+        else: #win
+            score = 6 + 2
+    if(opp=='B'):
+        if(you=='Y'):#draw
+            score = 3 + 2
+        elif(you=='X'):#lose
+            score = 0 + 1
+        else: #win
+            score = 6 + 3
+    if(opp=='C'):
+        if(you=='Z'):#draw
+            score = 3 + 3
+        elif(you=='Y'):#lose
+            score = 0 + 2
+        else: #win
+            score = 6 + 1
     total_score+= score
 print(total_score)
-
