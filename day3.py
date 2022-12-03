69acc4800edf23bf4d27264296b6986aedf29bb0
Aryaman-13 <aryaman.khandelwal@plaksha.edu.in>
2022-12-03 11:18:52 +0530

    add day3.py

---
+++ day3.py
@@ -0,0 +1,15 @@
+#This is code for day3 AOC
+file1 = open("day3.txt","r")
+data = file1.read().split('\n')
+common_list = []
+for i in range(len(data)):
+    word = data[i]
+    word1 = word[0:len(word)//2]
+    word2 = word[len(word)//2:len(word)]
+    a = list(set(word1)&set(word2))
+    for j in a:
+        if(j.isupper()==True):
+            common_list.append(ord(j)-38)
+        else:
+            common_list.append(ord(j)-96)
+print(sum(common_list))
