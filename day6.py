5f0d2649d7c2d5ea6b9c5f8dcf7196bbbecae564
Aryaman-13 <aryaman.khandelwal@plaksha.edu.in>
2022-12-06 11:16:03 +0530

    change the structure

--- day6.py
+++ day6.py
@@ -1,15 +1,20 @@
-#This is code for day6 AOC
 file1 = open("day6.txt","r")
 data = file1.read().split('\n')
-str = data[0]
-x = a = 0
-for i in range(4,len(str)):
-    sub_word = str[a:i]
-    l1 = sub_word[0]
-    l2 = sub_word[1]
-    l3 = sub_word[2]
-    l4 = sub_word[3]
-    if(sub_word.count(l1)==1 and sub_word.count(l2)==1 and sub_word.count(l3)==1 and sub_word.count(l4)==1):
-        print(i)
-        break
-    a+=1
+def get_index(data,y):
+    a = 0
+    str = data[0]
+    for i in range(y,len(str)):
+        sub_word = str[a:i]
+        count = 0
+        for j in sub_word:
+            if(sub_word.count(j)>1):
+                count+=1
+        if(count==0):
+            return i
+            break
+        a+=1
+print(get_index(data,4)) #soln for the first question
+print(get_index(data,14)) #soln for the second question
+
+
+
